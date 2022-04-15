f2652ea356e0b29ea3668839bae36d2952200867
iamthefastone <o.osisiogu@gmail.com>
2022-04-15 15:07:54 +0100

    Display scheme colors and hex values on page

--- script.js
+++ script.js
@@ -2,35 +2,31 @@ let seedColor = ""
 let selectedColorScheme = ""
 let colorsArr = []
 let colorSchemeHtml = ""
+let colorSchemeHexCode = ""
 const colorPickerForm = document.getElementById("color-picker-form")
 
 function renderColors() {
-    
-    for (let color of colorsArr) {
+    colorsArr.map((color)=>{
         colorSchemeHtml += `
-        <div id="color-bar" class="color-bar">
-            <img src="${color.image.bare}" alt="${color.name.value} HexCode ${color.name.closest_named_hex}">
-            <p>colors go here</p>
+        <div id="color-bar" class="color-bar" style="background-color:${color.hex.value}" aria-label="${color.name.value} HexCode is ${color.name.closest_named_hex}">
         </div>
         `
-        console.log(`The color is ${color.hex.value} and you can view the image here ${color.image.bare}`)
-    }
+        colorSchemeHexCode += `<div id="color-hex" class="color-hex one"><p>${color.hex.value}</p></div>`
+    })
     document.getElementById("color-bars-container").innerHTML = colorSchemeHtml
+    document.getElementById("color-hex-container").innerHTML = colorSchemeHexCode
+    colorSchemeHtml = ""
+    colorSchemeHexCode = ""
 }
 
 colorPickerForm.addEventListener("submit", (e) => {
     e.preventDefault()
     seedColor = document.getElementById("seed-color-picker").value.replace("#", "")
     selectedColorScheme = document.getElementById("select-color-scheme").value
-    console.log(seedColor + "  & " + selectedColorScheme)
-    console.log(`https://www.thecolorapi.com/scheme?hex=${seedColor}&format=html&mode=${selectedColorScheme}&count=5`)
-
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${selectedColorScheme}&count=5`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.colors)
             colorsArr = data.colors
             renderColors()
-            
         })
 })
