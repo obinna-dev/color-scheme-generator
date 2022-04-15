3640adf34a9adbd74f8af048a67410dfe19c4b7b
iamthefastone <o.osisiogu@gmail.com>
2022-04-15 17:39:20 +0100

    Style text

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let seedColor = ""
-let selectedColorScheme = ""
 let colorsArr = []
 let colorSchemeHtml = ""
 let colorSchemeHexCode = ""
@@ -8,10 +6,12 @@ const colorPickerForm = document.getElementById("color-picker-form")
 function renderColors() {
     colorsArr.map((color)=>{
         colorSchemeHtml += `
-        <div id="color-bar" class="color-bar" style="background-color:${color.hex.value}" aria-label="${color.name.value} HexCode is ${color.name.closest_named_hex}">
+        <div id="color-bar" class="color-bar" style="background-color:${color.hex.value}" 
+        aria-label="${color.name.value} HexCode is ${color.name.closest_named_hex}">
         </div>
         `
-        colorSchemeHexCode += `<div id="color-hex" class="color-hex one"><p>${color.hex.value}</p></div>`
+        colorSchemeHexCode += `
+        <div id="color-hex" class="color-hex one"><p>${color.hex.value}</p></div>`
     })
     document.getElementById("color-bars-container").innerHTML = colorSchemeHtml
     document.getElementById("color-hex-container").innerHTML = colorSchemeHexCode
@@ -21,8 +21,8 @@ function renderColors() {
 
 colorPickerForm.addEventListener("submit", (e) => {
     e.preventDefault()
-    seedColor = document.getElementById("seed-color-picker").value.replace("#", "")
-    selectedColorScheme = document.getElementById("select-color-scheme").value
+    const seedColor = document.getElementById("seed-color-picker").value.replace("#", "")
+    const selectedColorScheme = document.getElementById("select-color-scheme").value
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${selectedColorScheme}&count=5`)
         .then(res => res.json())
         .then(data => {
